67661b8248066d19f2d6b0ed06c3317c3804ab91
Dominik Eichler <dspam@ldkf.de>
2016-02-09 00:40:12 +0100

    Hello World

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$username="vudb";
-	$password="k1QFSTrIDs7TcwanJbzV";
-	$dbname="vudb";
-	$api_key="830d6e2d4d737d56aa1f94f717a477df";
-	$secret="1a05eab1f6dba7de78d59a6c94267464";
-	$server="localhost";
-?> 
