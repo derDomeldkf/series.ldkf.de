53a1b1821ffbdfbfbee5526aad5f41b2804195ae
Dominik Eichler <dspam@ldkf.de>
2016-02-09 13:50:24 +0100

    addes accounts

--- index.php
+++ index.php
@@ -111,7 +111,7 @@
 							echo'	<hr ALIGN="LEFT" style="background: red; height: 2px; width:180px"></div>';
 						}
 						else{
-							echo'	<hr ALIGN="LEFT" style="background: red; height: 2px; width:430px"></div>';
+							echo'	<hr ALIGN="LEFT" style="background: red; height: 2px; width:435px"></div>';
 						}
 					}
 					echo '<br>
