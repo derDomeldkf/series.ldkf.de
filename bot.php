de0f4efa0fc93bbf86899bda7c9abfc9abb0a550
Dominik Eichler <dspam@ldkf.de>
2016-04-12 18:39:43 +0200

    add telegram login

--- bot.php
+++ bot.php
@@ -1,13 +1,10 @@
 <?php
-	//$bot_id = "187000985:AAFhG3pmW3jBJ_ZJkcFzzMuB-WyqZ_cTvR8";
+	$bot_id = "187000985:AAFhG3pmW3jBJ_ZJkcFzzMuB-WyqZ_cTvR8";
 //$response = file_get_contents('php://input');
 
 
-//$url = 'https://api.telegram.org/bot'.$bot_id.'/sendMessage?chat_id=78597075&text=test'; 
-		//$result = file_get_contents($url);
-
-echo "tet";
-
+$url = 'https://api.telegram.org/bot'.$bot_id.'/sendMessage?chat_id=78597075&text=test'; 
+		$result = file_get_contents($url);
 
 
 ?>
