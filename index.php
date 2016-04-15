db95ab623427de5cbec3374ca134e7548acf9a67
Dominik Eichler <dspam@ldkf.de>
2016-04-15 18:47:02 +0200

    update

--- index.php
+++ index.php
@@ -37,6 +37,9 @@
 			$s=$_POST['staf'];
 			$e=$_POST['epi'];
 			$insert = $db->query("INSERT INTO serien (smax, emax, name) VALUES ('$s', '$e', '$name')"); 
+			$getid = $db->query("SELECT `id` FROM `serien` WHERE name LIKE '$name'"); 
+			$id= mysqli_fetch_assoc($getid)['id']
+			$insert = $db->query("INSERT INTO usersserie (id, user, e, s) VALUES ('$id', '$user_in', '0', '0')"); 
 			header('Location: ./');
 		}
 		$getid = $db->query("SELECT `id` FROM `serien`"); 
